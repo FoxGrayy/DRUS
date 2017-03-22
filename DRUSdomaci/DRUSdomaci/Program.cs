3749f70ae5a055a9eb5818213be5b314e265cc5e
FoxGrayy <lazicmilica24@gmail.com>
2017-03-22 23:41:06 +0100

    DRUS domaci

---
+++ DRUSdomaci/DRUSdomaci/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DRUSdomaci
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string userChoice = "T";
+           do
+           {
+            Console.WriteLine("Convert from Celsious to Fhrenheit!");
+            Console.WriteLine("To convert  Fhrenheit to Celsious  enter A");
+            Console.WriteLine("To convert Celsious to Fhrenheit enter E");
+            Console.WriteLine("To exit enter E");
+            userChoice = Console.ReadLine();
+            
+            
+                if (userChoice == "A")
+                {
+
+                    Console.WriteLine("Please enter Fhrenheit degrees you want to convert . ");
+                    string userInput = Console.ReadLine();
+
+                    TempConverter cel = new TempConverter(userInput);
+                    cel.FahrenheitToCelsius(userInput);
+                   //5 Console.ReadLine();
+                }
+
+                else if (userChoice == "B")
+                {
+                    Console.WriteLine("Please enter Celsious degrees you want to convert . ");
+                    string userInput = Console.ReadLine();
+
+                    TempConverter fhr = new TempConverter(userInput);
+                    fhr.CelsiusToFahrenheit(userInput);
+                   // Console.ReadLine();
+                }
+                else if (userChoice == "E")
+                {
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Please enter A or B or E.");
+                    Console.ReadKey();
+                }
+            } while (userChoice != "E");
+
+            //********************
+        }  
+    
+    }
+
+}
+
---
+++ DRUSdomaci/DRUSdomaci/TempConverter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace DRUSdomaci
+{
+    
+    class TempConverter
+    {
+        double valD; /// korisceno u TryParse
+        int valInt;
+
+        public TempConverter(){}
+
+        public string userInput = Console.ReadLine();
+        public TempConverter(string userInput)
+        {
+            this.userInput = userInput;
+        }
+
+
+        //conversion from Fahrenheit to Celsious -- Deduct 32, then multiply by 5, then divide by 9
+        public double FahrenheitToCelsius(string userInput) 
+        {
+           
+           double celsius = 0;
+           if(double.TryParse(userInput, out valD) || int.TryParse(userInput, out valInt))
+           {
+                celsius = (Convert.ToDouble(userInput) - 32) * 5 / 9;
+                Console.WriteLine("Temperature in Celsius is: " + celsius + " degree!");
+           }
+           
+           else
+           {
+               Console.WriteLine("Please enter a number!");
+           }
+           return celsius;
+           
+        }
+
+        //conversion from Celsious to Fahrenheit-- T(°F) = T(°C) × 9/5 + 32
+       public double CelsiusToFahrenheit(string userInput)
+       {
+          
+           double fahrenheit = 0;
+           if(double.TryParse(userInput, out valD) || int.TryParse(userInput, out valInt))
+           {
+               Console.WriteLine(userInput);
+               fahrenheit = Convert.ToDouble(userInput) * 1.8 + 32;
+               Console.WriteLine("Temperature in Fahrenheit is: " + fahrenheit + " degree!");
+           }
+           
+           else
+           {
+               Console.WriteLine("Please enter a number!");
+           }
+
+           return fahrenheit;
+       }
+
+
+      
+    }
+
+
+}
+           
+           
